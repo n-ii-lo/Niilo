6144997ac02f47821d845489f76b22d367c8d88f
n-ii-l-o <alyaska56@gmail.com>
2020-04-03 15:39:09 +0300

    'improved scroll arrow and slider'

--- Binar/js/custom.js
+++ Binar/js/custom.js
@@ -21,3 +21,24 @@ function parallax() {
 	var wScroll = $(window).scrollTop()
 	$('.parallax--bg').css('background-position', 'center ' + (wScroll * 0.75) + 'px');
 }
+
+(function () {
+	'use strict';
+
+	var btnScrollDown = document.querySelector('#scroll_down');
+
+	function scrollDown() {
+		let windowCoords = document.documentElement.clientHeight;
+		(function scroll() {
+			if (window.pageYOffset < windowCoords) {
+				window.scrollBy(0, 4);
+				setTimeout(scroll, 0);
+			}
+			if (window.pageYOffset > windowCoords) {
+				window.scrollTo(0, windowCoords);
+			}
+		})();
+	}
+
+	btnScrollDown.addEventListener('click', scrollDown);
+})();
